34a50c8e415ee217e8180577be06e4a778c0b55b
Orient Lu <lcdsdream@126.com>
2015-04-28 21:54:13 +0800

    加入按钮中断驱动 用于触发启动

--- configureMainWindow/main.cpp
+++ configureMainWindow/main.cpp
@@ -6,6 +6,14 @@
 #include <QTextCodec>
 #include "configureMainWindow.h"
 
+extern "C"
+{
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+
 int main(int argc, char *argv[])
 {
 	QApplication app(argc, argv);
@@ -16,6 +24,34 @@ int main(int argc, char *argv[])
 
 	app.setFont(QFont("WenQuanYi"));
 	ConfigureMainWindow mw;
-	mw.show();
-	return app.exec();
+	
+	int fd;
+	int val;
+	int key_value;
+	fd = ::open("/dev/orientButton",0);
+	if (fd<0)
+	{
+		printf("open error\n");
+		return -1;
+	}
+
+	//按钮驱动
+	while(1)
+	{
+		val = read(fd, (char*)&key_value, sizeof(key_value));
+		if(val < 0)
+		{
+			printf("read error\n");
+			continue;
+		}
+		if (key_value)
+		{
+			mw.show();
+			app.exec();
+		}
+	}
+	::close(fd);
+	return 0;
+}
+
 }
